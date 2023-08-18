06a783224db162d446bc9887159e8cb630f5770f
PraveenChamod <praveenchamod23@gmail.com>
2023-08-18 15:07:05 +0530

    Created React Store in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import weatherReducer from "./actions/weatherStatus";
+import "./App.css";
 import Dashboard from "./pages/Dashboard";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ViewWeather from "./pages/ViewWeather";
 
-function App() {
+const App = () => {
+  const store = configureStore({
+    reducer: {
+      weather: weatherReducer,
+    },
+  });
+
   return (
-    <Router>
-      <div className="my_app">
-        <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route exact path="/:CityCode" element={<ViewWeather />} />
-        </Routes>
-        <Footer/>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="my_app">
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/:CityCode" element={<ViewWeather />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;

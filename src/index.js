c2bf25e8c515f2a15bd1d8775262dbb6b05abde1
Praveen Chamod <praveenchamod23@gmail.com>
2023-09-22 16:21:20 +0530

    wrap the App with Auth0Provider.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-yehgmu7ilgvpg1zl.us.auth0.com"
+    clientId="aRWrtUWn1Lj40AkxefLC0basp2N3A5hd"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 reportWebVitals();
